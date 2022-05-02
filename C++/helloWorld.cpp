c4329ecc4b0bb45ad15b8b6a9d4c69b51b2b7c25
dantrr <dantrrthepirate@gmail.com>
2022-05-02 12:52:22 -0400

    Added hW cpp

---
+++ C++/helloWorld.cpp
@@ -0,0 +1,17 @@
+// Your First C++ Program
+
+#include <iostream>
+//include iostream allows low level computer control
+
+int main() {
+    //int main() function needed to start program
+
+std::cout << "Hello World!";
+// ; needed to end a line, std:cout is == print in Py, << needed followed by string: " "
+
+
+
+
+
+}
+
