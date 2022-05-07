c5621cc9cf594ec6abbaf6582e665e704a8d261b
Christopher Kennedy <teytey007@rocketmail.com>
2022-05-07 04:36:58 -0400

    usb upload

--- Python/Python for everybody.py
+++ Python/Python for everybody.py
@@ -1,2 +1,31 @@
 print("Hello World!")
 
+hoursStr = input("Enter hours worked: ")
+payRateStr = input("Enter your Pay Rate: ")
+
+try:
+    hours = float(hoursStr)
+    payRate = float(payRateStr)
+except:
+    hours = 0
+    payRate = 0
+    print("Use a number, not text.")
+grossPayStr = str(hours * payRate)
+print ("Your gross pay is $" + grossPayStr)
+
+#below is a user DEFINED function, starting with def
+# def example():
+#     print("test")
+
+#Arguments can specifify differences in same func. 
+
+# def example(lang)
+#     if lang == en
+#         print("en example")
+#     elif lang == es 
+#         print("es example")
+
+#return can be used to concatentate with str such as
+# def example()
+#     return "Hello"
+#print (example(), Bob)
