6f59931745823a83974f83effd61f2f5b11cc399
Christopher Kennedy <teytey007@rocketmail.com>
2022-05-27 09:59:15 -0400

    text update

--- Python/tipCalc.py
+++ Python/tipCalc.py
@@ -1,4 +1,4 @@
 bill = float(input("What is your bill amount? "))
 percent = float(input("What percentage would you like to tip? "))
 tip = (bill*percent/100)
-print(f"Your tip should be {tip}")
+print(f"Your tip should be ${tip}")
