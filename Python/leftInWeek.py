10c4ea6bc611f405be5e0a39f0e501b48143782d
dantrr <dantrrthepirate@gmail.com>
2022-05-31 23:31:06 -0400

    update

--- Python/leftInWeek.py
+++ Python/leftInWeek.py
@@ -1,14 +1,11 @@
 days = 6
 hours = 8
+currentHour = 40
+#as of 0800 day 5
 
 totalHours = days * hours
-
-currentHour = 24
-#as of 0800 day 3
-
 percentComp = currentHour/totalHours*100
 hoursleft = totalHours-currentHour
 perRound = round(percentComp , 2)
-
 print(f"The week is {perRound}% complete.")
 print(f"There are {hoursleft} hours left in the week.")
