356fcb68272f33c69e4cf7256c9d4aeae5e7f70e
dantrr <dantrrthepirate@gmail.com>
2022-06-03 15:48:31 -0400

    Updated rounding

--- Python/tipCalc.py
+++ Python/tipCalc.py
@@ -1,4 +1,5 @@
 bill = float(input("What is your bill amount? "))
 percent = float(input("What percentage would you like to tip? "))
 tip = (bill*percent/100)
-print(f"Your tip should be ${tip}")
+tipRound = round(tip, 2)
+print(f"Your tip should be ${tipRound}")
