b7b04f92ac0bb5c539728bc18836cec17fff74e2
dantrr <dantrrthepirate@gmail.com>
2022-08-01 07:57:42 -0400

    Ruby lessons to self

--- Ruby/helloWorld.rb
+++ Ruby/helloWorld.rb
@@ -2,4 +2,26 @@
 puts "hello world"
 
 #print in ruby, is basically a non-newline print
-print "hello world"
+print "hello world\n"
+
+=begin
+this is a 
+multi-line
+comment
+=end
+x=8
+puts x
+
+# variable with a capital letter are constants, and their value cannot be changed. 
+
+mynum = 42
+
+# MyNum = 42
+
+# MyNum = 0 #this gives us an error
+
+
+# an example of an fstring could be implemented like: 
+age = 42
+
+puts "he is #{age} years old"
