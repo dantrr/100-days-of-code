1359e0ab061e04eac762a056f218ddacb6ab8c66
dantrr <dantrrthepirate@gmail.com>
2022-09-03 22:54:10 -0400

    new py

---
+++ Python/retirementNumber.py
@@ -0,0 +1,5 @@
+perHun = 30000
+monthlySpend = input("How much do you need every month? ")
+flotSpend = float(monthlySpend)
+maths = (flotSpend / 100) * perHun
+print(f"You need to invest about ${maths}.")
