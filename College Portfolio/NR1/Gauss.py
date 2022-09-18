41ef1aeaf917637e57d46d6bfdae268641a4f3b8
Christopher Kennedy <teytey007@rocketmail.com>
2022-09-18 03:51:30 -0400

    Comments

--- College Portfolio/NR1/Gauss.py
+++ College Portfolio/NR1/Gauss.py
@@ -6,3 +6,5 @@ half = endFlot / 2
 floatAdd = float(addEm)
 gaussAns = floatAdd * half
 print(f"The sum of all numbers in between are {gaussAns}.")
+
+#possible attempt 2: do a iteration loop to find answer the long way.
