ac3993c3be498c3fe18167aae12a4b22ba1e2f08
Christopher Kennedy <teytey007@rocketmail.com>
2022-09-18 03:50:01 -0400

    Attempt at Gauss problem.

---
+++ College Portfolio/NR1/Gauss.py
@@ -0,0 +1,8 @@
+initNum = input("First Number? ")
+endNum = input("End Number? ")
+addEm = initNum + endNum
+endFlot = float(endNum)
+half = endFlot / 2
+floatAdd = float(addEm)
+gaussAns = floatAdd * half
+print(f"The sum of all numbers in between are {gaussAns}.")
